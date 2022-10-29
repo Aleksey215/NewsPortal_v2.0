6faed33f25a4bf19735a5de17ff68500cd8ce926
Aleksey215 <kaloha215@gmail.com>
2022-10-29 12:34:24 +1000

    added news app in INSTALLED_APPS

--- NewsPortal2/config/settings.py
+++ NewsPortal2/config/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'news.apps.NewsConfig',
 ]
 
 MIDDLEWARE = [
