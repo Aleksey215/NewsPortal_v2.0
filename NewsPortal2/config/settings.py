b09870cbbdcb3f86c1af98f9c1ceeabae00e66f8
Aleksey215 <kaloha215@gmail.com>
2022-10-29 12:30:46 +1000

    Created requirements.txt Configured the creation of environment variables Created new app "news"

--- NewsPortal2/config/settings.py
+++ NewsPortal2/config/settings.py
@@ -9,8 +9,15 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
+
+# для переменных окружения
+from dotenv import load_dotenv
+
+load_dotenv()
+env_path = Path('config')/'.env'
+load_dotenv(dotenv_path=env_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#q%)jhvdr8_mv3@#^=sk%_n_6%64-+^vbg43h7p%un%d(^uepq'
+SECRET_KEY = os.getenv('KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
