48d30e35c840b279fd58e9f5be190ee4d61c8a5a
Aleksey215 <kaloha215@gmail.com>
2022-11-25 21:50:55 +1000

    added sending emails to category subscribers when creating a new publication

--- NewsPortal2/news/signals.py
+++ NewsPortal2/news/signals.py
@@ -1,19 +1,30 @@
-# для получения сигнала о создании нового объекта
-import time
-
-from django.db.models.signals import post_save, m2m_changed
+from django.db.models.signals import m2m_changed
 # декоратор для подключения функции к нужному сигналу
 from django.dispatch import receiver
 from django.core.mail import EmailMultiAlternatives
-from time import sleep
+from django.template.loader import render_to_string
 
 from .models import *
+from config.settings import DEFAULT_FROM_EMAIL
 
 
 @receiver(signal=m2m_changed, sender=PostCategory)
 def notify_subscribers(sender, instance, **kwargs):
-    recipients = []
-    categories = instance.category.all()
-    for category in categories:
-        print(category.subscribers.all().email)
-
+    post_categories = Category.objects.prefetch_related("subscribers").filter(post=instance)
+    for category in post_categories:
+        for subscriber in category.subscribers.all():
+            subscriber_name = subscriber.username
+            subscriber_email = subscriber.email
+            html_content = render_to_string(
+                'email/new_post_mail.html',{
+                    'post': instance,
+                    'content': instance.content[:50],
+                }
+            )
+            msg = EmailMultiAlternatives(
+                subject=f'Здравствуй, {subscriber_name}. Новая статья в вашем разделе!',
+                from_email=DEFAULT_FROM_EMAIL,
+                to=[subscriber_email]
+            )
+            msg.attach_alternative(html_content, 'text/html')
+            msg.send()
