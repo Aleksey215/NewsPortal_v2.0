b02b42600b47f9b2329407b5d865cc7de316aac2
Aleksey215 <kaloha215@gmail.com>
2022-11-30 19:56:53 +1000

    added weekly mailing of letters to subscribers

--- NewsPortal2/config/celery.py
+++ NewsPortal2/config/celery.py
@@ -6,17 +6,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 app = Celery('NewsPortal2')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
-app.conf.beat_schedule = {
-        # Executes every Monday morning at 7:30 a.m.
-        'add-every-monday-morning': {
-            'task': 'tasks.weekly_send_mail_task',
-            'schedule': crontab(hour=19, minute=45, day_of_week=2),
-            # 'args': (weekly_emails_to_subscribers),
-        },
-    }
-
 app.autodiscover_tasks()
 
 
-
+app.conf.beat_schedule = {
+    'weekly_send_mails': {
+        'task': 'news.tasks.weekly_send_mail_task',
+        'schedule': crontab(minute='*/1'),
+    }
+}
