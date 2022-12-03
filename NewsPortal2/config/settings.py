97a43fe3335e9d6ab21e05896800523034b5b98f
Aleksey215 <kaloha215@gmail.com>
2022-12-03 21:42:35 +1000

    changed database on PostgreSQL

--- NewsPortal2/config/settings.py
+++ NewsPortal2/config/settings.py
@@ -106,11 +106,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'newsportal',
+        'USER': 'newsportal_admin',
+        'PASSWORD': 1234,
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
 }
 
 # Password validation
